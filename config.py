78ac74585a19f6896f7fa2ff9efa32bc5550bafd
Dmitry Mikhaylov <good8815585@yandex.ru>
2024-03-18 18:32:32 +0300

    fix logging

--- config.py
+++ config.py
@@ -1,25 +1,32 @@
 import logging
 import os
-
+from logging.handlers import RotatingFileHandler
 from dotenv import load_dotenv
 
-logging.basicConfig(
-    filename='app.log',
-    level=logging.INFO,
-    format='%(asctime)s - %(levelname)s - %(message)s',
-    datefmt='%Y-%m-%d %H:%M:%S'
-)
+load_dotenv()
 
 logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+formatter = logging.Formatter(
+    '%(asctime)s - %(levelname)s - %(message)s', datefmt='%Y-%m-%d %H:%M:%S'
+)
+
+log_file = 'app.log'
+
+max_file_size = 1024 * 1024 * 50  # 50 MB
+
+file_handler = RotatingFileHandler(
+    log_file, maxBytes=max_file_size, backupCount=3
+)
+file_handler.setLevel(logging.INFO)
+file_handler.setFormatter(formatter)
+
+logger.addHandler(file_handler)
 
 REQUEST_DELAY_INTERVAL = 3600  # request delay interval in seconds
 LIMIT = 10  # limit of result list
-
-
 DATABASE_NAME = 'currency_database.db'
-
-
-load_dotenv()
 TOKEN = os.getenv('TOKEN')  # telegram bot token
 URL = os.getenv("URL")  # url for parsing data
 USER_AGENT = os.getenv("USER_AGENT")
